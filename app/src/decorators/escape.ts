4b8cb3ce017ba6776119a20d6719d9dbea7afc39
João Brogna <brogna2000@gmail.com>
2023-11-14 13:02:02 -0300

    first commit

---
+++ app/src/decorators/escape.ts
@@ -0,0 +1,18 @@
+export function escape(
+    target: any,
+    propertyKey: string,
+    descriptor: PropertyDescriptor,
+){
+    const metodoOriginal = descriptor.value;
+    descriptor.value = function(...args: any[]){ // any[] é igual a Array<any>
+
+        let  retorno = metodoOriginal.apply(this, args);
+        if(typeof retorno === 'string'){
+            console.log(`@escape em ação na classe ${this.constructor.name} para o metodo ${propertyKey} `)
+            retorno = retorno.replace(/<script>[\s\S]*?<\/script>/, '');
+        }
+        return retorno;
+    }
+
+    return descriptor;
+}
